c35a9cda17992ea1d5d5fb97c5f6ebb56d78c56c
Phil Austin <paustin@eos.ubc.ca>
2020-08-17 13:42:02 -0700

    fix installs

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -55,8 +55,13 @@ c.Spawner.mem_limit = '10G'
 ## Services
 c.JupyterHub.services = [
     {
-        'name': 'cull_idle',
+        'name': 'idle-culler',
         'admin': True,
-        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
-    },
+        'command': [
+            sys.executable,
+            '-m', 'jupyterhub_idle_culler',
+            '--timeout=3600'
+        ],
+    }
 ]
+
